b931d412b4809854417896c0df71115b04ec3e91
gmahima <mahimagangavarapu@gmail.com>
2020-09-17 17:37:00 +0530

    move funcs

--- stores/usePlayerStore.js
+++ stores/usePlayerStore.js
@@ -17,57 +17,69 @@ const initialPlayers = [
 const handleLoadPlayers = (set, get) => {
     let p = get().players
     console.log(p)
-  }
-  
+}
+const handleAddPlayer = (set, get, name) =>{
+  set(state => {
+    let n = [...state.players]
+    let p ={};
+    p.name = name;
+    p.id = (n.length+1).toString();
+    p.score = 0;
+    n.push(p)
+    return ({
+      players: n
+    })
+  })
+}
+
+const handleIncrementPlayerScore = (set, get, id) => {
+  set(state => {
+    console.log(state.players)
+    const n = [...state.players]
+    const player = n.find(p => p.id === id)
+    console.log(player)
+    player.score = player.score+10;
+    if(player.score > state.highScore) {
+      const sounds = get().sounds;
+      const i = Math.floor(Math.random()*sounds.length); 
+      console.log(player.name+ " says: "+ sounds[i])
+      return ({
+        players: n,
+        highScore: player.score
+      })
+    }
+    return ({
+      players: n
+    })
+
+  })
+}
+
+const handleDecrementPlayerScore = (set, get, id) => {
+  set(state => {
+    console.log(state.players)
+    const n = [...state.players]
+    const player = n.find(p => p.id === id)
+    console.log(player)
+    player.score = player.score-10;
+    return ({
+      players: n
+    })
+
+  })
+}
+
+const sounds = ['yay', 'wohoo', 'hah!', 'I am the best']
   
 const playerStore = (set,get) => ({
     players: initialPlayers,
     loadPlayers: () => {handleLoadPlayers(set, get)},
-    addPlayer: (name => set(state => {
-      let n = [...state.players]
-      let p ={};
-      p.name = name;
-      p.id = (n.length+1).toString();
-      p.score = 0;
-      n.push(p)
-      return ({
-        players: n
-      })
-    })),
-    sounds: ['yay', 'wohoo', 'hah!', 'I am the best'],
+    addPlayer: (name) => {handleAddPlayer(set,get, name)},
+    sounds: sounds,
     highScore: 0,
     setHighScore: (s => set({highScore: s})),
-    incrementPlayerScore: (id) => (set(state => {
-      console.log(state.players)
-      const n = [...state.players]
-      const player = n.find(p => p.id === id)
-      console.log(player)
-      player.score = player.score+10;
-      if(player.score > state.highScore) {
-        const sounds = get().sounds;
-        const i = Math.floor(Math.random()*sounds.length); 
-        console.log(player.name+ " says: "+ sounds[i])
-        return ({
-          players: n,
-          highScore: player.score
-        })
-      }
-      return ({
-        players: n
-      })
-  
-    })),
-    decrementPlayerScore: (id) => (set(state => {
-      console.log(state.players)
-      const n = [...state.players]
-      const player = n.find(p => p.id === id)
-      console.log(player)
-      player.score = player.score-10;
-      return ({
-        players: n
-      })
-  
-    })),
+    incrementPlayerScore: (id) => {handleIncrementPlayerScore(set,get, id)},
+    decrementPlayerScore: (id) => {handleDecrementPlayerScore(set,get, id)},
     deleteEverything: () => (set ({}, true)) // true replaces state model instead of merging it
   })
   
